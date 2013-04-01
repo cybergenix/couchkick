8973f62f2ebd6015f43d735555e9d8a424e3850c
Tiago Martins <tiagom87@gmail.com>
2013-04-01 15:56:13 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Couchkick::Application.config.secret_token = '46b718fe3222066452d8027a9a07197096d78c5d8051eb7f10b6a518fffe2f6fb85b37e372ea7b57e3498906a18e821b78494945a624682a89790073f32e8193'
