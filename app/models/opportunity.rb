2134f91a35c799a10401c3feae043e412bab91bf
Tiago Martins <tiagom87@gmail.com>
2013-04-10 15:21:26 +0100

    Further changes

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,5 +1,5 @@
 class Opportunity < ActiveRecord::Base
-  attr_accessible :entrepreneur_id, :opportunity_type, :usertype, :tag_list, :text
+  attr_accessible :entrepreneur_id, :opportunity_type, :usertype, :tag_list, :text, :info
   acts_as_taggable
   belongs_to :entrepreneur
   has_many :applicants, as: :commentable
